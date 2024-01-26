ea619bf47cd40864622755dba2d73f9251cdc802
Andre Torres <andregtorres@gmail.com>
2024-01-26 18:15:55 +0100

    fix processing script

--- processing/fullProcessing.sh
+++ processing/fullProcessing.sh
@@ -2,4 +2,5 @@
 python histogram_requests.py
 cd /home/agtorres/homeMeteo/plots
 ./ftpPlots.sh
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' http://homemeteo.atorres.eu/dataStat.php
 curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' http://homemeteo.atorres.eu/deleteLogs.php
