49a0a0233dfa1c434225ff264419b820ab81ac92
Andre Torres <andregtorres@gmail.com>
2024-07-06 18:52:26 +0200

    fix deleteLogs

--- web/deleteLogs.php
+++ web/deleteLogs.php
@@ -2,7 +2,7 @@
     //Connect to database
     include("include/dbConn.php");
 
-    $interval = "5 DAY";
+    $interval = "5";
     //Get current date
     $today_s = date("Y-m-d");
     $today = new DateTime($today_s);
@@ -13,11 +13,18 @@
           $interval = test_input($_POST['interval']);
         }
         echo "id: ".$id."\n";
-        echo "interval: ".$interval."\n";
-        $stmt = $conn->prepare("DELETE FROM homeMeteoLogs WHERE host = ? AND timestamp <= ( CURDATE() - INTERVAL ?)");
-        $stmt -> bind_param("ss", $id, $interval);
-        $stmt->execute();
-        $stmt -> close();
+        echo "interval: ".$interval." DAY\n";
+        try {
+          $stmt = $conn->prepare("DELETE FROM homeMeteoLogs WHERE host = ? AND timestamp <= (CURDATE() - INTERVAL ? DAY)");
+          $stmt -> bind_param("ss", $id, $interval);
+          $stmt->execute();
+          $stmt -> close();
+        }catch (Exception $e){
+          $error = $e->getMessage();
+          echo "ERROR: ";
+          echo $error;
+          echo "\n";
+        }
         echo "DELETED ROWS";
     } else {
         echo "Error - invalid host id\n";
