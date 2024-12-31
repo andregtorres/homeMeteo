999c6be2afa51250edb9bfd9c62bb5ce0afc9bf0
Andre Torres <andregtorres@gmail.com>
2024-12-31 10:56:08 +0100

    add processing for the 2 devices

--- processing/fullProcessing.sh
+++ processing/fullProcessing.sh
@@ -7,6 +7,12 @@
 ####
 
 source /home/agtorres/homeMeteo/processing/httpConn.pw
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/binData.php
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/dataStat.php
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/deleteLogs.php
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/binData.php
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/dataStat.php
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=0' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/deleteLogs.php
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=1' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/binData.php
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=1' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/dataStat.php
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=1' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/deleteLogs.php
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=2' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/binData.php
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=2' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/dataStat.php
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'id=2' --user $USER:$PASSWORD http://homemeteo.atorres.eu/API/deleteLogs.php
