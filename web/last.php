45e042f5659e24dca36d01435c4de575a3c3a28c
Andre Torres <andregtorres@gmail.com>
2025-09-26 22:58:45 +0200

    add json last

--- web/last.php
+++ web/last.php
@@ -7,7 +7,7 @@
   //Connect to database
   include("include/dbConn.php");
 
-  $sql1 = "SELECT timestamp, temp, humi FROM homeMeteoLogs WHERE host = 0 AND timestamp >= ( CURDATE() - INTERVAL 0 DAY)";
+  $sql1 = "SELECT timestamp, temp, humi FROM homeMeteoLogs WHERE host = 1 AND timestamp >= ( CURDATE() - INTERVAL 0 DAY)";
   $result1 = $conn->query($sql1);
   if ($result1->num_rows > 0) {
 		//this is a bit stupid, but i forgot to add an auto incrementing id to the table :(
---
+++ web/last_json.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+//Connect to database
+include("include/dbConn.php");
+
+$sql1 = "SELECT timestamp, temp, humi FROM homeMeteoLogs WHERE host = 1 AND timestamp >= ( CURDATE() - INTERVAL 0 DAY)";
+$result1 = $conn->query($sql1);
+if ($result1->num_rows > 0) {
+    //this is a bit stupid, but i forgot to add an auto incrementing id to the table :(
+    while($row = $result1->fetch_assoc()) {
+        $time = $row["timestamp"];
+        $temp = $row["temp"];
+        $humi = $row["humi"];
+    }
+    $data = [ 'temp' => round($temp/100, 1), 'humi' => round($humi/100, 0), "status" => 1 ];    
+
+} else {
+    $data = [ 'temp' => 0, 'humi' => 0, "status" => 0 ];    
+}
+echo json_encode($data);
+
+$conn->close();
+?>
+
